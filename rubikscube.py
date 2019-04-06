e18fe5d9650eb2a9fa0bce7e33ff9aa6fc999556
EliasKomar <elias.komar@ctemplar.com>
2019-04-06 17:43:07 -0400

    main loop fix

--- rubikscube.py
+++ rubikscube.py
@@ -12,26 +12,29 @@ cube_radius = 125
 phi = pi/4
 theta = pi/4
 
-
 pygame.init()
 surface = pygame.display.set_mode((750,750))
-running = True
 draw_rubikscube(surface,cube_radius,phi,-theta,[250,250],size,cube,lines)
 pygame.display.flip()
 
-##while running:
-##    next_move = input()
-##    if next_move == right:
-##        rigth(cube,0)
-##    if next_move == left:
-##        left(cube,0)
-##    if next_move == up:
-##        up(cube,0)
-##    if next_move == down:
-##        down(cube,0)
-##    if next_move == front:
-##        front(cube,0)
-##    if next_move == back:
-##        back(cube,0)
-##    draw_rubikscube(surface,cube_radius,phi,theta,[250,250],size,cube,lines)
-##    pygame.display.flip()
+run = True
+while run:
+    move = input()
+    if not(move in ['r','l','u','d','f','b']):
+        move = last_move
+    if move == 'r':
+        right(cube,0,size)
+    if move == 'l':
+        left(cube,0,size)
+    if move == 'u':
+        up(cube,0,size)
+    if move == 'd':
+        down(cube,0,size)
+    if move == 'f':
+        front(cube,0,size)
+    if move == 'b':
+        back(cube,0,size)
+    draw_rubikscube(surface,cube_radius,phi,-theta,[250,250],size,cube,lines)
+    pygame.display.flip()
+    last_move = move
+    move = ''
